b690d6b9a84c7eb2d057ce77a46406f3118f718c
João Teixeira <jldteixeira@gmail.com>
2025-10-14 12:27:41 +0100

    Update dependencies

--- examples/ember/vite.config.js
+++ examples/ember/vite.config.js
@@ -3,12 +3,15 @@ import { ember, extensions } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
-    ember(),
+    ember({
+      // Only include tests in test mode
+      ...(mode === 'test' && { test: true })
+    }),
     babel({
       babelHelpers: 'inline',
       extensions
     })
   ]
-});
+}));
